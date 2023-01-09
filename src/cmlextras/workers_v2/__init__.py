10bb07c47139a3f0953874413ed81738994bcdca
Peter Ableda <peter.ableda@cloudera.com>
2023-01-09 11:02:00 +0000

    Update year in Copyright

--- src/cmlextras/workers_v2/__init__.py
+++ src/cmlextras/workers_v2/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2022 Cloudera. All Rights Reserved.
+# Copyright 2023 Cloudera. All Rights Reserved.
 #
 # This file is licensed under the Apache License Version 2.0
 # (the "License"). You may not use this file except in compliance
